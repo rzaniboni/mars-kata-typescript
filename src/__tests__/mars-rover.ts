fbf97ce86aea63f5e03e1f0bd7a6d6e3be32b94f
Roberto Zaniboni <zaniboni@gmail.com>
2020-10-22 12:45:56 +0200

    Add forward command

--- src/__tests__/mars-rover.ts
+++ src/__tests__/mars-rover.ts
@@ -1,11 +1,9 @@
-import { Rover, Direction } from '../mars-rover'
+import { Rover, Direction } from "../mars-rover";
 
 test("it works", () => {});
 
-describe("Mars Rover", () => { 
-
+describe("Mars Rover", () => {
   describe("It is given the initial starting point (x,y) and the direction (N,S,E,W) a rover is facing.", () => {
-
     it("should set starting point", () => {
       const rover = Rover(10, 10);
 
@@ -18,36 +16,51 @@ describe("Mars Rover", () => {
 
       let expectedPosition = { x: 0, y: 0 };
       expect(rover.state().position).toEqual(expectedPosition);
-    })
+    });
 
     it("should set direction (N,S,E,W)", () => {
       const rover = Rover(0, 0, "E");
 
       let expectedDirection: Direction = "E";
       expect(rover.state().direction).toEqual(expectedDirection);
-    })
+    });
 
     it("should set default direction to N when not assigned", () => {
       const rover = Rover();
 
       let expectedDirection: Direction = "N";
       expect(rover.state().direction).toEqual(expectedDirection);
-    })
-  })
+    });
+  });
 
-
-  
   it("Rover receives a character array of commands.", () => {
+    const rover = Rover();
+    const result = rover.commands("FFBB");
 
-    const rover = Rover()
-    const result = rover.commands("FFBB")
-
-    let expectedCommands: string[] = [
-      "F", "F", "B", "B"
-    ];
+    let expectedCommands: string[] = ["F", "F", "B", "B"];
     expect(result).toEqual(expectedCommands);
-  });  
+  });
+
+  describe("it can move forward when receiving command F", () => {
+    it("when oriented N moves towards N on y", () => {
+      const rover = Rover(0, 0, "N");
+      rover.execute("F");
+
+      let expectedPosition = { x: 0, y: 1 };
+      let expectedDirection = "N";
+      expect(rover.state().position).toEqual(expectedPosition);
+      expect(rover.state().direction).toEqual(expectedDirection);
+    });
 
 
+    it("when oriented E moves towards E on x", () => {
+      const rover = Rover(0, 0, "E");
+      rover.execute("FF");
 
-})
+      let expectedPosition = { x: 2, y: 0 };
+      let expectedDirection = "E";
+      expect(rover.state().position).toEqual(expectedPosition);
+      expect(rover.state().direction).toEqual(expectedDirection);
+    });
+  });
+});
--- src/mars-rover.ts
+++ src/mars-rover.ts
@@ -13,6 +13,44 @@ export interface Result {
   direction: Direction;
 }
 
+export type Command = "F"
+
+function exhaustiveCheck(_: never) {}
+
+/*
+const moveForward = (rover: RoverState): RoverState => {
+  const { direction, x, y} = rover
+  switch (direction) {
+    case "N":
+      return { ...rover, y: y + 1 };
+    case "E":
+      return { ...rover, x: x + 1 };
+    case "S":
+      return { ...rover, y: y - 1 };
+    case "W":
+      return { ...rover, x: x - 1 };
+    default:
+      exhaustiveCheck(direction);
+  }
+};
+*/
+
+const moveForwardMap: Record<Direction, (RoverState) => (RoverState)> = {
+  N: (state) => ({...state, y: state.y + 1}),
+  E: (state) => ({...state, x: state.x + 1}),
+  S: (state) => ({...state, y: state.y - 1}),   
+  W: (state) => ({...state, x: state.x - 1}),   
+} 
+
+
+const reducer = (state: RoverState, command: Command): RoverState => {
+  switch (command) {
+    case "F": 
+     return moveForwardMap[state.direction](state)
+  } 
+}
+
+
 export function Rover(
   x = 0,
   y = 0,
@@ -29,14 +67,25 @@ export function Rover(
     return commandSequence.split("");
   };
 
+
+  const execute = (commandSequence: string): void => {
+    roverState = commands(commandSequence).reduce((state, command) => {
+      return (reducer(state, command as Command))
+    }, roverState)
+  }
+
+
   const state = (): Result => ({
     direction: roverState.direction,
     position: { x: roverState.x, y: roverState.y}
   })
 
+
+
   return {
     state,
-    commands
+    commands,
+    execute
   }
 }
 
